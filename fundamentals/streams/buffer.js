e21ddd97a8aca7f5de739c08b523673678ee9ccd
bernardo <joseberk2@gmail.com>
2026-02-12 09:52:59 -0300

    feat: add quick explanation to what a buffer represent in node

---
+++ fundamentals/streams/buffer.js
@@ -0,0 +1,7 @@
+const buffer = Buffer.from("Bernardo")
+
+console.log(buffer)
+console.log(buffer.toJSON())
+
+// Um buffer eh uma estrutura de dados que armazena os dados em formato Binario ( em bytes )
+// facilia e otimiza a leitura para a maquina para as streams do node
